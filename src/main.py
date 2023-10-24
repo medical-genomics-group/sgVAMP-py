aea2ca4693e00d28af598ded420ec2e91c7a2f8a
JakubBajzik <jakub.bajzik@gmail.com>
2023-10-24 15:51:31 +0200

    fixing printing metrics bug in main

--- src/main.py
+++ src/main.py
@@ -13,28 +13,33 @@ print("...Test run of VAMP for summary statistics\n", flush=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("-ld_file", "--ld-file", help = "Path to LD matrix")
 parser.add_argument("-r_file", "--r-file", help = "Path to XTy file")
+parser.add_argument("-true_signal_file", "--true-signal-file", help = "Path to true signal file")
 parser.add_argument("-N", "--N", help = "Number of samples")
 parser.add_argument("-M", "--M", help = "Number of markers")
 parser.add_argument("-iterations", "--iterations", help = "Number of iterations", default=10)
-parser.add_argument("-h2", "--h2", help = "Heritability used in simulations", default=0.5)
+parser.add_argument("-h2", "--h2", help = "Heritability used in simulations", default=0.8)
 parser.add_argument("-lam", "--lam", help = "Sparsity (lambda) used in simulations", default=0.5)
 args = parser.parse_args()
 
 # Input parameters
 ld_fpath = args.ld_file
 r_fpath = args.r_file
+true_signal_fpath = args.true_signal_file
 M = int(args.M) # Number of markers
 N = int(args.N) # Number of samples
 iterations = int(args.iterations)
 h2 = float(args.h2) # heritability for simulations
 lam = float(args.lam) # Sparsity for simulations
 
-
 # Loading LD matrix and XTy vector
 print("...loading LD matrix and XTy vector", flush=True)
 R = scipy.sparse.load_npz(ld_fpath).toarray()
 r = np.load(r_fpath)
-print("Data loaded. Shapes: ", R.shape, r.shape, flush=True)
+print("LD matrix and XTy loaded. Shapes: ", R.shape, r.shape, flush=True)
+
+# Loading true signals
+beta = np.load(true_signal_fpath)
+print("True signals loaded. Shape: ", beta.shape, flush=True)
 
 # sgVAMP init
 sgvamp = VAMP(lam=lam, rho=0.5, gam1=100, gamw=1/h2)
@@ -50,15 +55,9 @@ te = time.time()
 print("sgVAMP total running time: %0.4fs \n" % (te - ts), flush=True)
 
 # Print metrics
-R2s = []
 corrs = []
 for it in range(iterations):
-    yhat = X @ xhat1[it]
-    R2 = r2_score(y, yhat) # R squared metric
-    R2s.append(R2)
-
     corr = np.corrcoef(xhat1[it].squeeze(), beta.squeeze()) # Pearson correlation coefficient of xhat1 and true signal beta
     corrs.append(corr[0,-1])
-print("R2 over iterations: \n", R2s)
 print("Corr(x1hat,beta) over iterations: \n", corrs)
 print("gamw over iterations: \n", gamw, flush=True)
