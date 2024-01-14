8db469dc51b42cc3743e04f6be2b3d56c1829db9
JakubBajzik <jakub.bajzik@gmail.com>
2024-01-14 08:23:19 +0100

    First parallel MPI version working

--- src/sgvamp.py
+++ src/sgvamp.py
@@ -12,16 +12,17 @@ import struct
 import logging
 
 class VAMP:
-    def __init__(self, K, rho, gamw, gam1, prior_vars, prior_probs, out_dir, out_name):
+    def __init__(self, K, rho, gamw, gam1, prior_vars, prior_probs, out_dir, out_name, comm):
         self.eps = 1e-32
         self.K = K
         self.rho = rho
         self.gamw = gamw
-        self.gam1 = np.full(K, gam1)
+        self.gam1 = gam1#np.full(K, gam1)
         self.lam = 1 - prior_probs[0]
         self.sigmas = np.array(prior_vars[1:]) # a vector containing variances of different groups
         self.omegas = np.array([ p / sum(prior_probs[1:]) for p in prior_probs[1:]])
         self.setup_io(out_dir, out_name)
+        self.comm = comm
 
     def setup_io(self, out_dir, out_name):
         self.out_dir = out_dir
@@ -90,12 +91,16 @@ class VAMP:
     def infer(self,R,r,M,N,iterations,cg_maxit=500,learn_gamw=True, lmmse_damp=True):
 
         # Initialization
-        r1 = np.array(r).reshape((self.K,M))
+        rank = self.comm.Get_rank()
+        r = r.reshape((M,1))
+        r1 = r.reshape((M,1))
+        r1s = np.zeros((self.K, M))
         xhat1 = np.zeros((M,1)) # signal estimates in Denoising step
         xhat2 = np.zeros((M,1)) # signal estimates in LMMSE step
-        Sigma2_u_prev = [np.zeros((M,1)) for i in range(self.K)]
+        Sigma2_u_prev = np.zeros((M,1))
         rho = self.rho # Damping factor
         gam1 = self.gam1
+        gam1s = np.zeros(self.K)
         gamw = self.gamw # Precision of noise
         xhat1s = []
         I = scipy.sparse.identity(M) # Identity matrix
@@ -104,71 +109,88 @@ class VAMP:
         alpha2 = 0
 
         for it in range(iterations):
-            logging.info(f"\n -----ITERATION {it} -----")
+            if rank == 0:
+                logging.info(f"\n -----ITERATION {it} -----")
             
+            # Collect gam1s and r1s
+            #logging.info(f"rank {rank}: gam1={gam1}")
+            gam1s[rank] = gam1
+            r1s[rank,:] = r1.squeeze()
+            for i in range(self.K):
+                #if i != rank:
+                gam1s[i] = self.comm.bcast(gam1s[i], root=i)
+                r1s[i,:] = self.comm.bcast(r1s[i,:], root=i)
+            
+            #if rank == 0:
+            #    logging.info(f"gam1s={gam1s}")
+
+            if rank == 0:
+                logging.info(f"...Data from all ranks collected")
+
             # Denoising
-            logging.info("...Denoising")
+            if rank == 0:
+                logging.info(f"...Denoising")
             
             xhat1_prev = xhat1
             alpha1_prev = alpha1
             
-            xhat1 = np.array([self.denoiser_meta(r1[:,i], gam1) for i in range(M)]).reshape((M,1))
+            xhat1 = np.array([self.denoiser_meta(r1s[:,i], gam1s) for i in range(M)]).reshape((M,1))
 
             xhat1 = rho * xhat1 + (1 - rho) * xhat1_prev # apply damping on xhat1
 
             xhat1s.append(xhat1)
-            self.write_xhat_to_file(it, xhat1)
+            
+            if rank == 0:
+                self.write_xhat_to_file(it, xhat1)
 
-            alpha1 = np.mean(np.array([self.der_denoiser_meta(r1[:,i], gam1) for i in range(M)]))
+            alpha1 = np.mean(np.array([self.der_denoiser_meta(r1s[:,i], gam1s) for i in range(M)]))
             alpha1 = rho * alpha1 + (1 - rho) * alpha1_prev # apply damping on alpha1
 
             # LMMSE for multiple cohorts
-            logging.info("...LMMSE")
+            logging.info(f"...LMMSE cohort {rank}")
 
             xhat2_prev = xhat2
             alpha2_prev = alpha2
+            r1 = r1.reshape((M,1))
+            xhat1 = xhat1.reshape((M,1))
+            gam2 = gam1 * (1 - alpha1) / alpha1
+            r2 = (xhat1 - alpha1 * r1) / (1 - alpha1)
 
-            for i in range(self.K):
+            A = (gamw * R + gam2 * I) # Sigma2 = A^(-1)
+            mu2 = (gamw * r + gam2 * r2)
 
-                logging.info(f"...processing cohort {i}")
-
-                r1i = r1[i,:].reshape((M,1))
-                gam2 = gam1[i] * (1 - alpha1) / alpha1
-                r2 = (xhat1 - alpha1 * r1i) / (1 - alpha1)
-
-                A = (gamw * R[i] + gam2 * I) # Sigma2 = A^(-1)
-                mu2 = (gamw * r[i] + gam2 * r2)
-                
-                # Conjugate gradient for solving linear system A^(-1) @ mu2 = Sigma2 @ mu2
-                xhat2, ret = con_grad(A, mu2, maxiter=cg_maxit, x0=xhat2_prev)
+            # Conjugate gradient for solving linear system A^(-1) @ mu2 = Sigma2 @ mu2
+            xhat2, ret = con_grad(A, mu2, maxiter=cg_maxit, x0=xhat2_prev)
             
-                if ret > 0: logging.info(f"WARNING: CG 1 convergence after {ret} iterations not achieved!")
-                xhat2.resize((M,1))
+            if ret > 0: 
+                logging.info(f"Rank {rank} WARNING: CG 1 convergence after {ret} iterations not achieved!")
+            xhat2.resize((M,1))
 
-                if lmmse_damp:
-                    xhat2 = rho * xhat2 + (1 - rho) * xhat2_prev # damping on xhat2
+            if lmmse_damp:
+                xhat2 = rho * xhat2 + (1 - rho) * xhat2_prev # damping on xhat2
 
-                # Generate iid random vector [-1,1] of size M
-                u = binomial(p=1/2, n=1, size=M) * 2 - 1
+            # Generate iid random vector [-1,1] of size M
+            u = binomial(p=1/2, n=1, size=M) * 2 - 1
 
-                # Hutchinson trace estimator
-                # Sigma2 = (gamw * R + gam2 * I)^(-1)
-                # Conjugate gradient for solving linear system (gamw * R + gam2 * I)^(-1) @ u
+            # Hutchinson trace estimator
+            # Sigma2 = (gamw * R + gam2 * I)^(-1)
+            # Conjugate gradient for solving linear system (gamw * R + gam2 * I)^(-1) @ u
 
-                Sigma2_u, ret = con_grad(A,u, maxiter=cg_maxit, x0=Sigma2_u_prev[i])
-                Sigma2_u_prev[i] = Sigma2_u
+            Sigma2_u, ret = con_grad(A,u, maxiter=cg_maxit, x0=Sigma2_u_prev)
+            Sigma2_u_prev = Sigma2_u
 
-                if ret > 0: logging.info(f"WARNING: CG 2 convergence after {ret} iterations not achieved!")
+            if ret > 0: 
+                logging.info(f"Rank {rank} WARNING: CG 2 convergence after {ret} iterations not achieved!")
 
-                TrSigma2 = u.T @ Sigma2_u # Tr[Sigma2] = u^T @ Sigma2 @ u 
+            TrSigma2 = u.T @ Sigma2_u # Tr[Sigma2] = u^T @ Sigma2 @ u 
 
-                alpha2 = gam2 * TrSigma2 / M
-                if lmmse_damp:
-                    alpha2 = rho * alpha2 + (1 - rho) * alpha2_prev # damping on alpha2
-                gam1[i] = gam2 * (1 - alpha2) / alpha2
-                r1[i,:] = np.squeeze((xhat2 - alpha2 * r2) / (1-alpha2))
+            alpha2 = gam2 * TrSigma2 / M
+            if lmmse_damp:
+                alpha2 = rho * alpha2 + (1 - rho) * alpha2_prev # damping on alpha2
+            gam1 = gam2 * (1 - alpha2) / alpha2
+            r1 = np.squeeze((xhat2 - alpha2 * r2) / (1-alpha2))
 
-                # Write parameters for current cohort to csv file
-                self.write_params_to_file([it, gamw, gam1[i], gam2, alpha1, alpha2], i)
+            # Write parameters for current cohort to csv file
+            self.write_params_to_file([it, gamw, gam1, gam2, alpha1, alpha2], rank)
 
         return xhat1s
