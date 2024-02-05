63b1ea3c7661f342c9390d31af3079012ff803c4
JakubBajzik <jakub.bajzik@gmail.com>
2024-02-05 11:35:13 +0100

    Negative values in MLE update

--- src/sgvamp.py
+++ src/sgvamp.py
@@ -170,12 +170,12 @@ class VAMP:
             x0[-1] = self.gam
 
         x, _, ier, _ = scipy.optimize.fsolve(func=self.Lagrangian_der, x0=x0, args=(omega0,sigma2,r1s,gam1s), full_output=True)
-
+        #logging.debug(f"x={x}")
         if ier != 1:
             if rank == 0:
                 logging.info(f"WARNING: fsolve not converged. No prior update!")
             return
-        elif any(x[:-1]) < 0:
+        elif any(s <= 0 for s in x[:-1]):
             if rank == 0:
                 logging.info(f"WARNING: Negative values in MLE. No prior update!")
             return
